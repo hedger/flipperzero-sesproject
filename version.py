2690283e2c66078b1b747650f50f00eecad8eb6e
hedger <hedger@nanode.su>
2022-05-08 21:30:31 +0400

    Removed dirty flag from version; reformatted py script

--- version.py
+++ version.py
@@ -4,53 +4,58 @@ import subprocess
 import os
 from datetime import date
 
-def exec_git(args):
-  cmd = ['git']
-  cmd.extend(args.split(' '))
-  return subprocess.check_output(cmd, cwd="..", stderr=subprocess.STDOUT).strip().decode()
 
-commit = exec_git('rev-parse --short HEAD') or 'unknown'
+def exec_git(args):
+    cmd = ["git"]
+    cmd.extend(args.split(" "))
+    return (
+        subprocess.check_output(cmd, cwd="..", stderr=subprocess.STDOUT)
+        .strip()
+        .decode()
+    )
+
+
+commit = exec_git("rev-parse --short HEAD") or "unknown"
 
 dirty = False
 try:
-  exec_git('diff --quiet')
+    exec_git("diff --quiet")
 except subprocess.CalledProcessError as e:
-  if e.returncode == 1:
-    dirty = True
-    commit += '-dirty'
+    if e.returncode == 1:
+        dirty = True
 
-branch = exec_git('rev-parse --abbrev-ref HEAD') or 'unknown'
-branch_num = exec_git('rev-list --count HEAD') or 'n/a'
+branch = exec_git("rev-parse --abbrev-ref HEAD") or "unknown"
+branch_num = exec_git("rev-list --count HEAD") or "n/a"
 build_date = date.today().isoformat()
 
 try:
-  version = exec_git('describe --tags --abbrev=0 --exact-match')
+    version = exec_git("describe --tags --abbrev=0 --exact-match")
 except subprocess.CalledProcessError:
-  version = 'unknown'
+    version = "unknown"
 
 defines = [
-  '-DGIT_COMMIT=\\"%s\\"' % commit,
-  '-DGIT_BRANCH=\\"%s\\"' % branch,
-  '-DGIT_BRANCH_NUM=\\"%s\\"' % branch_num,
-  '-DVERSION=\\"%s\\"' % version,
-  '-DBUILD_DATE=\\"%s\\"' % build_date,
-  '-DBUILD_DIRTY=%s' % (dirty and 1 or 0)
+    '-DGIT_COMMIT=\\"%s\\"' % commit,
+    '-DGIT_BRANCH=\\"%s\\"' % branch,
+    '-DGIT_BRANCH_NUM=\\"%s\\"' % branch_num,
+    '-DVERSION=\\"%s\\"' % version,
+    '-DBUILD_DATE=\\"%s\\"' % build_date,
+    "-DBUILD_DIRTY=%s" % (dirty and 1 or 0),
 ]
 
 
-new_defines = ' '.join(defines)
+new_defines = " ".join(defines)
 current_defines = None
 
 try:
-  with open('version.inc', 'r') as file:
-    current_defines = file.read()
+    with open("version.inc", "r") as file:
+        current_defines = file.read()
 except EnvironmentError:
-  pass
+    pass
 
 if current_defines != new_defines:
-  with open('version.inc', 'w') as file:
-    file.write(new_defines)
-  os.utime('../lib/toolbox/version.c', None)
-  print("Version information updated")
+    with open("version.inc", "w") as file:
+        file.write(new_defines)
+    os.utime("../lib/toolbox/version.c", None)
+    print("Version information updated")
 else:
-  print("Version information hasn't changed")
+    print("Version information hasn't changed")
